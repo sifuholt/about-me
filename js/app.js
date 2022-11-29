5787116e98c5d0740484d13f3c69c41b0b74ff43
Kenneth Holt <kingholt@gmail.com>
2022-11-29 15:10:27 -0800

    updating files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+console.log('Hey world hey!!');
+
+let userName = prompt('what is your name?');
+
+alert(`welcome to my site ${userName}! Pleast answer the following questions with a yes/no or y/n`);
+
+let questionOneGuess = prompt('Do i have a daughter?').toLowerCase();
+
+if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
+  alert('you got it right');
+} else if( questionOneGuess === 'no' || questionOneGuess === 'n'){
+  alert('sorry, you got it wrong');
+}
+
