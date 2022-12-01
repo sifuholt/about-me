7e66d8d29098449ded09de19a0511aa88e95e0ea
Kenneth Holt <kingholt@gmail.com>
2022-11-30 18:53:58 -0800

    added notes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # about-me
+
+## author: ken
+
+## links and resources
+
+grabbed this code from stackoverflow
+collabed with jacob
+
+reflections and comments
+
--- js/app.js
+++ js/app.js
@@ -2,14 +2,17 @@
 
 console.log('Hey world hey!!');
 
+let score = 0;
+
 let userName = prompt('Hi, What is your name?');
 
 alert(`Welcome to my site ${userName}! Please answer the following questions with a yes/no or y/n`);
 
-let questionOneGuess = prompt('Have I ever done anything with computers before?').toLowerCase();
+let questionOneGuess = prompt('Is this my first time ever doing anyting with computers?').toLowerCase();
 
 if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
   alert('You got it right');
+  score++;
 } else if( questionOneGuess === 'no' || questionOneGuess === 'n'){
   alert('Sorry, not sure that response is correct.');
 }
@@ -18,6 +21,7 @@ let questionTwoGuess = prompt('Was I ever in the military?').toLowerCase();
 
 if(questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
   alert('You got it right');
+  score++;
 } else if( questionTwoGuess === 'no' || questionTwoGuess === 'n'){
   alert('Sorry, not sure that response is correct.');
 }
@@ -26,6 +30,7 @@ let questionThreeGuess = prompt('Am I happily married with children?').toLowerCa
 
 if(questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
   alert('You got it right');
+  score++;
 } else if( questionThreeGuess === 'no' || questionThreeGuess === 'n'){
   alert('Sorry, not sure that response is correct.');
 }
@@ -34,6 +39,7 @@ let questionFourGuess = prompt('Do I live in Oregon?').toLowerCase();
 
 if(questionFourGuess === 'yes' || questionFourGuess === 'y'){
   alert('You got it right');
+  score++;
 } else if( questionFourGuess === 'no' || questionFourGuess === 'n'){
   alert('Sorry, not sure that response is correct.');
 }
@@ -42,9 +48,49 @@ let questionFiveGuess = prompt('Am I having fun learning this?').toLowerCase();
 
 if(questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
   alert('You got it right');
+  score++;
 } else if( questionFiveGuess === 'no' || questionFiveGuess === 'n'){
   alert('Sorry, not sure that response is correct.');
 }
 
-alert(`Thanks for coming to my site and getting to know me better ${userName}, I look forward to us getting to know one another better`);
 
+let attempt = 4;
+let myNum = 7;
+
+for(let i=0; i < attempt; i++){
+  let questionSixGuess = prompt('Please guess a number between 0 and 10');
+
+  if(myNum == questionSixGuess) {
+    alert('Wow! I can not believe you got that right so fast!');
+    break;
+  } else if (questionSixGuess <= 6){
+    alert('Try a higher number...');
+  }else if (questionSixGuess >= 8){
+    alert('Nice try, but I am looking for something lower...');
+  }else {
+    alert('Nice tries...the number was 7!');
+  }
+}
+let bookAnswer = ['three body problem', 'infinite jest', 'ulysses'];
+
+let guesses = 6;
+
+for(let i=0; i < guesses; i++){
+  let questionSevenGuess = prompt('How about a game? Of the books I listed, what would you think are my top three?').toLowerCase();
+  for (let b = 0; b < bookAnswer.length; b++) {
+    if (questionSevenGuess == bookAnswer[b]) {
+      alert('Wow!!!...you either really know me, or are very good at guessing!');
+      score++;
+      i = 6;
+      break;
+    }
+  }
+}
+
+
+
+
+
+alert(`Thanks for coming to my site and getting to know me better ${userName}, I look forward to us getting to know one another better!`);
+
+alert(`Outstanding! ${userName} you got ${score} out of 5 correct! That is some good guessing...you should play the lottery!`);
