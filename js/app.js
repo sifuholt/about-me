491de470da14f97a3d914507547aaa3db003ff32
Kenneth Holt <kingholt@gmail.com>
2022-12-01 18:23:16 -0800

    added functions

--- js/app.js
+++ js/app.js
@@ -8,88 +8,105 @@ let userName = prompt('Hi, What is your name?');
 
 alert(`Welcome to my site ${userName}! Please answer the following questions with a yes/no or y/n`);
 
-let questionOneGuess = prompt('Is this my first time ever doing anyting with computers?').toLowerCase();
+function questionOne (){
+  let questionOneGuess = prompt('Is this my first time ever doing anyting with computers?').toLowerCase();
 
-if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
-  alert('You got it right');
-  score++;
-} else if( questionOneGuess === 'no' || questionOneGuess === 'n'){
-  alert('Sorry, not sure that response is correct.');
-}
-
-let questionTwoGuess = prompt('Was I ever in the military?').toLowerCase();
-
-if(questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
-  alert('You got it right');
-  score++;
-} else if( questionTwoGuess === 'no' || questionTwoGuess === 'n'){
-  alert('Sorry, not sure that response is correct.');
-}
-
-let questionThreeGuess = prompt('Am I happily married with children?').toLowerCase();
-
-if(questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
-  alert('You got it right');
-  score++;
-} else if( questionThreeGuess === 'no' || questionThreeGuess === 'n'){
-  alert('Sorry, not sure that response is correct.');
-}
-
-let questionFourGuess = prompt('Do I live in Oregon?').toLowerCase();
-
-if(questionFourGuess === 'yes' || questionFourGuess === 'y'){
-  alert('You got it right');
-  score++;
-} else if( questionFourGuess === 'no' || questionFourGuess === 'n'){
-  alert('Sorry, not sure that response is correct.');
-}
-
-let questionFiveGuess = prompt('Am I having fun learning this?').toLowerCase();
-
-if(questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
-  alert('You got it right');
-  score++;
-} else if( questionFiveGuess === 'no' || questionFiveGuess === 'n'){
-  alert('Sorry, not sure that response is correct.');
-}
-
-
-let attempt = 4;
-let myNum = 7;
-
-for(let i=0; i < attempt; i++){
-  let questionSixGuess = +prompt('Please guess a number between 0 and 10');
-
-  if(myNum === questionSixGuess) {
-    alert('Wow! I can not believe you got that right so fast!');
-    break;
-  } else if (questionSixGuess <= 6){
-    alert('Try a higher number...');
-  }else if (questionSixGuess >= 8){
-    alert('Nice try, but I am looking for something lower...');
-  }else {
-    alert('Nice tries...the number was 7!');
+  if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
+    alert('You got it right');
+    score++;
+  }else if( questionOneGuess === 'no' || questionOneGuess === 'n'){
+    alert('Sorry, not sure that response is correct.');
   }
 }
+questionOne();
 
+function questionTwo (){
+  let questionTwoGuess = prompt('Was I ever in the military?').toLowerCase();
 
-let bookAnswer = ['three body problem', 'infinite jest', 'ulysses'];
+  if(questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
+    alert('You got it right');
+    score++;
+  } else if( questionTwoGuess === 'no' || questionTwoGuess === 'n'){
+    alert('Sorry, not sure that response is correct.');
+  }
+}
+questionTwo();
 
-let guesses = 6;
+function questionThree(){
+  let questionThreeGuess = prompt('Am I happily married with children?').toLowerCase();
 
-for(let i=0; i < guesses; i++){
-  let questionSevenGuess = prompt('How about a game? Of the books I listed, what would you think are my top three?').toLowerCase();
-  for (let b = 0; b < bookAnswer.length; b++) {
-    if (questionSevenGuess === bookAnswer[b]) {
-      alert('Wow!!!...you either really know me, or are very good at guessing!');
-      score++;
-      i = 6;
+  if(questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
+    alert('You got it right');
+    score++;
+  } else if( questionThreeGuess === 'no' || questionThreeGuess === 'n'){
+    alert('Sorry, not sure that response is correct.');
+  }
+}
+questionThree();
+
+function questionFour(){
+  let questionFourGuess = prompt('Do I live in Oregon?').toLowerCase();
+
+  if(questionFourGuess === 'yes' || questionFourGuess === 'y'){
+    alert('You got it right');
+    score++;
+  } else if( questionFourGuess === 'no' || questionFourGuess === 'n'){
+    alert('Sorry, not sure that response is correct.');
+  }
+}
+questionFour();
+
+function questionFive(){
+  let questionFiveGuess = prompt('Am I having fun learning this?').toLowerCase();
+
+  if(questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
+    alert('You got it right');
+    score++;
+  } else if( questionFiveGuess === 'no' || questionFiveGuess === 'n'){
+    alert('Sorry, not sure that response is correct.');
+  }
+}
+questionFive();
+
+function questionSix(){
+  let attempt = 4;
+  let myNum = 7;
+
+  for(let i=0; i < attempt; i++){
+    let questionSixGuess = +prompt('Please guess a number between 0 and 10');
+
+    if(myNum === questionSixGuess) {
+      alert('Wow! I can not believe you got that right so fast!');
       break;
+    } else if (questionSixGuess <= 6){
+      alert('Try a higher number...');
+    }else if (questionSixGuess >= 8){
+      alert('Nice try, but I am looking for something lower...');
+    }else {
+      alert('Nice tries...the number was 7!');
     }
   }
 }
+questionSix();
 
+function questionSeven(){
+  let bookAnswer = ['three body problem', 'infinite jest', 'ulysses'];
 
+  let guesses = 6;
+
+  for(let i=0; i < guesses; i++){
+    let questionSevenGuess = prompt('How about a game? Of the books I listed, what would you think are my top three?').toLowerCase();
+    for (let b = 0; b < bookAnswer.length; b++) {
+      if (questionSevenGuess === bookAnswer[b]) {
+        alert('Wow!!!...you either really know me, or are very good at guessing!');
+        score++;
+        i = 6;
+        break;
+      }
+    }
+  }
+}
+questionSeven();
 
 
 
